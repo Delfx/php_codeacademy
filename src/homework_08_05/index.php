1726d6d27c2b88d2a44e4a9bb24c21e84448d3e2
Delfx <motiejunas.lukas@gmail.com>
2022-08-08 19:19:22 +0300

    updated homework

--- src/homework_08_05/index.php
+++ src/homework_08_05/index.php
@@ -3,7 +3,9 @@
 require "index.phtml";
 
 $params_needed = [
-    'first_name', 'last_name', 'additional_information'
+    'first_name' => 'First Name', 
+    'last_name' => 'Last Name', 
+    'additional_information' => 'Additional information'
 ];
 
 // https://stackoverflow.com/a/35665021
@@ -11,9 +13,9 @@ $params_needed = [
 $array = $_GET;
 
 if(count($array) != count(array_filter($array))){
-    foreach ($params_needed as $value) {
-        if (empty($_GET["{$value}"])) {
-            echo "Please fill field {$value} <br>";
+    foreach ($params_needed as $key => $value) {
+        if (empty($_GET["{$key}"])) {
+            echo "<br> Please fill field {$value} <br>";
         }
     }
 }else{
