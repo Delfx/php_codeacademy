cbd2593f4264fa1c316ccc80c89c45f82978ea89
Delfx <motiejunas.lukas@gmail.com>
2022-08-08 20:30:15 +0300

    updated task homework

--- src/homework_08_05/index.php
+++ src/homework_08_05/index.php
@@ -2,24 +2,25 @@
 
 require "index.phtml";
 
-$params_needed = [
+$paramsNeeded = [
     'first_name' => 'Vardas',
     'last_name' => 'Pavarde',
-    'additional_information' => 'Papildomas Informacija'
+    'additional_information' => 'Papildomas Informacija',
 ];
+
 
 // https://stackoverflow.com/a/35665021
 
 $array = $_GET;
 
 if (count($array) != count(array_filter($array))) {
-    foreach ($params_needed as $key => $value) {
+    foreach ($paramsNeeded as $key => $value) {
         if (empty($_GET["{$key}"])) {
             echo "<br> Please fill field {$value} <br>";
         }
     }
 } else if (empty($_GET)) {
-    echo '<br> Please fill all fields';
+    echo '<br> Užpildykite visus laukus';
 } else {
     echo '<br> <h2 style = "color: green"> Your form has been sended we will contact you very soon </h2>';
 }
